c5820e6f7359d6239f30c9edc6ef88eb480d1330
Guo Jin <gitjin@outlook.com>
2024-12-31 20:21:29 +0800

    update

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/csguojin/depcalldemog
+
+go 1.21
+
+require github.com/csguojin/depcalldemof v0.0.0-20241231121857-6e90a5292aab
---
+++ main.go
@@ -0,0 +1,12 @@
+package depcalldemog
+
+import (
+	"fmt"
+
+	"github.com/csguojin/depcalldemof"
+)
+
+func MyFunc1() {
+	fmt.Println("depcalldemog.MyFunc1")
+	depcalldemof.MyFunc1()
+}
